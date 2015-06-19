61496c43324c58dec52df025855ce8312ce14d03
Michal Fojtik <mfojtik@redhat.com>
2015-06-19 12:14:15 +0200

    Remove trap in ./hack/build.sh

--- hack/build.sh
+++ hack/build.sh
@@ -12,27 +12,10 @@ function squash {
   $HOME/.local/bin/docker-scripts squash -f $base ${IMAGE_NAME}
 }
 
-# TODO: Remove this hack once Docker 1.5 is in use,
-# which supports building of named Dockerfiles.
-function docker_build {
-  TAG=$1
-  DOCKERFILE=$2
-
-  if [ -n "$DOCKERFILE" -a "$DOCKERFILE" != "Dockerfile" ]; then
-    # Swap Dockerfiles and setup a trap restoring them
-    mv Dockerfile Dockerfile.centos7
-    mv "${DOCKERFILE}" Dockerfile
-    trap "mv Dockerfile ${DOCKERFILE} && mv Dockerfile.centos7 Dockerfile" ERR RETURN
-  fi
-
-  docker build -t ${TAG} . && trap - ERR
-  [ -z "${SKIP_SQUASH}" ] && squash
-
-  return 0
-}
-
 if [ "$OS" == "rhel7" -o "$OS" == "rhel7-candidate" ]; then
-  docker_build ${IMAGE_NAME} Dockerfile.rhel7
+  docker build -t ${IMAGE_NAME} -f Dockerfile.rhel7 .
 else
-  docker_build ${IMAGE_NAME}
+  docker build -t ${IMAGE_NAME} .
 fi
+
+[ -z "${SKIP_SQUASH}" ] && squash
