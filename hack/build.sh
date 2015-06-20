ea522eb9768976ed475a41d802f394c996bd89d9
Ben Parees <bparees@redhat.com>
2015-06-20 14:46:26 -0400

    fix squashing logic

--- hack/build.sh
+++ hack/build.sh
@@ -8,14 +8,15 @@ IMAGE_NAME=openshift/base-${OS}
 function squash {
   # install the docker layer squashing tool
   easy_install --user docker-scripts==0.4.1
-  base=$(awk '/^FROM/{print $2}' Dockerfile)
+  base=$(awk '/^FROM/{print $2}' $1)
   $HOME/.local/bin/docker-scripts squash -f $base ${IMAGE_NAME}
 }
 
 if [ "$OS" == "rhel7" -o "$OS" == "rhel7-candidate" ]; then
   docker build -t ${IMAGE_NAME} -f Dockerfile.rhel7 .
+  [ -z "${SKIP_SQUASH}" ] && squash Dockerfile.rhel7
 else
   docker build -t ${IMAGE_NAME} .
+  [ -z "${SKIP_SQUASH}" ] && squash Dockerfile
 fi
 
-[ -z "${SKIP_SQUASH}" ] && squash
