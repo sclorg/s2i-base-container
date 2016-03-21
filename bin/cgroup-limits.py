b67b0dad117e0ce470d61488811d402848c7d37f
Martin Nagy <nagy.martin@gmail.com>
2016-03-21 16:57:10 +0100

    Add documentation and usage notes

--- bin/cgroup-limits.py
+++ bin/cgroup-limits.py
@@ -1,12 +1,38 @@
 #!/usr/bin/python
 
+"""
+Script for parsing cgroup information
+
+This script will read some limits from the cgroup system and parse
+them, printing out "VARIABLE=VALUE" on each line for every limit that is
+successfully read. Output of this script can be directly fed into
+bash's export command. Recommended usage from a bash script:
+
+    set -o errexit
+    export_vars=$(cgroup-limits.py)
+    export $export_vars
+
+Variables currently supported:
+    MAX_MEMORY_LIMIT_IN_BYTES
+        Maximum possible limit MEMORY_LIMIT_IN_BYTES can have. This is
+        currently constant value of 9223372036854775807.
+    MEMORY_LIMIT_IN_BYTES
+        Maximum amount of user memory in bytes. If this value is set
+        to the same value as MAX_MEMORY_LIMIT_IN_BYTES, it means that
+        there is no limit set. The value is taken from
+        /sys/fs/cgroup/memory/memory.limit_in_bytes
+    NUMBER_OF_CORES
+        Number of detected CPU cores that can be used. This value is
+        calculated from /sys/fs/cgroup/cpuset/cpuset.cpus
+"""
+
 from __future__ import print_function
 import sys
 
 env_vars = {}
 
 
-def read_file(path):
+def _read_file(path):
     try:
         with open(path, 'r') as f:
             return f.read().strip()
@@ -15,7 +41,11 @@ def read_file(path):
 
 
 def get_memory_limit():
-    limit = read_file('/sys/fs/cgroup/memory/memory.limit_in_bytes')
+    """
+    Read memory limit, in bytes.
+    """
+
+    limit = _read_file('/sys/fs/cgroup/memory/memory.limit_in_bytes')
     if limit is None:
         print("Warning: Can't detect memory limit from cgroups",
               file=sys.stderr)
@@ -24,9 +54,13 @@ def get_memory_limit():
 
 
 def get_number_of_cores():
+    """
+    Read number of CPU cores.
+    """
+
     core_count = 0
 
-    line = read_file('/sys/fs/cgroup/cpuset/cpuset.cpus')
+    line = _read_file('/sys/fs/cgroup/cpuset/cpuset.cpus')
     if line is None:
         print("Warning: Can't detect number of CPU cores from cgroups",
               file=sys.stderr)
@@ -41,9 +75,10 @@ def get_number_of_cores():
 
     env_vars['NUMBER_OF_CORES'] = str(core_count)
 
-get_memory_limit()
-get_number_of_cores()
+if __name__ == "__main__":
+    get_memory_limit()
+    get_number_of_cores()
 
-print("MAX_MEMORY_LIMIT_IN_BYTES=9223372036854775807")
-for item in env_vars.items():
-    print("=".join(item))
+    print("MAX_MEMORY_LIMIT_IN_BYTES=9223372036854775807")
+    for item in env_vars.items():
+        print("=".join(item))
