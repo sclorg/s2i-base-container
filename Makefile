01aa90636bbb1983c0cd26614f65a16b0530f08b
Marek Skalický <mskalick@redhat.com>
2017-03-20 14:05:27 +0100

    Change default image name

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ else
 	OS := centos7
 endif
 
-BASE_IMAGE_NAME = "base"
+BASE_IMAGE_NAME = "s2i-base"
 
 script_env = \
 	SKIP_SQUASH=$(SKIP_SQUASH)                      \
--- hack/build.sh
+++ hack/build.sh
@@ -13,7 +13,7 @@ test -z "$BASE_IMAGE_NAME" && {
     BASE_IMAGE_NAME="${BASE_DIR_NAME#s2i-}"
 }
 
-NAMESPACE="openshift/"
+NAMESPACE="centos/"
 
 # Cleanup the temporary Dockerfile created by docker build with version
 trap "rm -f ${DOCKERFILE_PATH}.version" SIGINT SIGQUIT EXIT
